eda2845b0bc693684ac55ed7d0e2163b0d104387
Elvin Deras <ederas@gmail.com>
2021-02-16 01:04:14 -0600

    fixing

--- src/FuseSmallFS.cs
+++ src/FuseSmallFS.cs
@@ -66,7 +66,7 @@ namespace OperatingSystem{
 			
 			string name = "";
 			
-			for(int i=0; i<SECTOR_SIZE;i+=ENTRY_SIZE)
+			for(int i=0; i<FuseDevice.SECTOR_SIZE;i+=ENTRY_SIZE)
 			{
 				Array.Copy(dirDevice,i,entry,0,ENTRY_SIZE);
 				
